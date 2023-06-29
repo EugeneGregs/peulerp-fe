7880821c720c2310acf8f57c584f8c78ec10b209
eochieng <eochieng@microsoft.com>
2023-06-29 19:18:26 +0300

    Add product selection for purchase

--- src/MainPage/Purchase/AddPurchase.jsx
+++ src/MainPage/Purchase/AddPurchase.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Calendar,
   Plus,
@@ -11,113 +11,175 @@ import {
 import { Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Select2 from "react-select2-wrapper";
 import "react-select2-wrapper/css/select2.css";
-import { Table } from "antd";
-// import "bootstrap-select"
-// import "bootstrap-select/dist/css/bootstrap-select.min.css";
-// import "bootstrap-select/dist/js/bootstrap-select.min.js";
-// import 'bootstrap-select/dist/js/bootstrap-select.min.js';
-// import 'bootstrap-select/dist/css/bootstrap-select.min.css';
+import Table from "../../EntryFile/datatable";
+import Select from "react-select";
+import axios from "axios";
+import Modal from "react-bootstrap/Modal";
 
-const options = [
-  { id: 1, text: "Select", text: "Select" },
-  { id: 2, text: "Supplier", text: "Supplier" },
+const paymentStatus = [
+  { value: 1, label: "Paid" },
+  { value: 2, label: "Pending" },
+  { value: 3, label: "Partial" },
 ];
-const options1 = [
-  { id: 1, text: "Select", text: "Select" },
-  { id: 2, text: "Supplier Name", text: "Supplier Name" },
-];
-const options2 = [
-  { id: 1, text: "choose Status", text: "choose Status" },
-  { id: 2, text: "Inprogess", text: "Inprogess" },
-  { id: 3, text: "Completed", text: "Completed" },
-];
+
 const deleteRow = () => {
   $(document).on("click", ".delete-set", function () {
     $(this).parent().parent().hide();
   });
 };
 
-$(function() {
-  $('.selectpicker').selectpicker();
-});
+const baseUrl = "http://localhost:5071";
+
 const AddPurchase = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [products, setProducts] = useState([]);
+  const [productOptions, setProductOptions] = useState([]);
+  const [supplier, setSupplier] = useState([]);
+  const [supplierOptions, setSupplierOptions] = useState([]);
+  const [reffereceNo, setReffereceNo] = useState("");
+  const [purchaseProducts, setPurchaseProducts] = useState([]);
+  const [grandTotal, setGrandTotal] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [show, setShow] = useState(false);
+  const [editingPrduct, setEditingProduct] = useState({});
 
-  const [data] = useState([
-    {
-      id: 1,
-      image: EarpodIcon,
-      productName: "Apple Earpods",
-      qty: "10.00",
-      purchasePrice: "2000.00",
-      discount: "500.00",
-      tax: "0.00",
-      taxAmount: "0.00",
-      unitCost: "2000.00",
-      totalCost: "2000.00",
-    },
-    {
-      id: 2,
-      image: MacbookIcon,
-      productName: "Macbook Pro",
-      qty: "15.00",
-      purchasePrice: "6000.00",
-      discount: "100.00",
-      tax: "0.00",
-      taxAmount: "0.00",
-      unitCost: "1000.00",
-      totalCost: "1000.00",
-    },
-  ]);
+  const handSaveEdit = () => {
+    let purchasedProductsClone = [...purchaseProducts];
+    const index = purchasedProductsClone.findIndex(p => p.id === editingPrduct.id);
+    purchasedProductsClone[index] = editingPrduct;
+    setPurchaseProducts(purchasedProductsClone);
+  }
+
+  const handleClose = () => {
+    setShow(false);    
+    setEditingProduct({});
+  };
+
+  const handleShow = () => setShow(true);
+
+  const handleEdit = (product) => {
+    setEditingProduct(product);
+    handleShow();
+  };
+
+  const handleDetailsChange = (e, field) => {
+    const { value } = e.target;
+
+    if(field === "quantity") {
+      const { purchasePrice } = editingPrduct;
+      setEditingProduct({ ...editingPrduct, quantity: value, totalCost: purchasePrice * value });
+    }
+
+    if(field === "purchasePrice") {
+      const { quantity } = editingPrduct;
+      setEditingProduct({ ...editingPrduct, purchasePrice: value, totalCost: quantity * value });
+    }
+  }
+
+  useEffect(() => {
+    Promise.all([fetchProducts(), fetchSuppliers()]);
+    setReffereceNo(generateRefferenceNumber());
+  }, []);
+
+  const generateRefferenceNumber = () =>
+    ([1e7] + -1e3 + -4e3 + -8e3 + -1e11)
+      .replace(/[018]/g, (c) =>
+        (
+          c ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+      )
+      .substring(25)
+      .toUpperCase();
+
+  useEffect(() => {
+    console.log(products);
+    let options = products.map((product) => {
+      return { value: product.id, label: product.name };
+    });
+    setProductOptions(options);
+    console.log(options);
+  }, [products]);
+
+  useEffect(() => {
+    let supplierOptions = supplier.map((supplier) => {
+      return { value: supplier.id, label: supplier.name };
+    });
+    setSupplierOptions(supplierOptions);
+  }, [supplier]);
+
+  const fetchProducts = async () => {
+    const productsRes = await axios(`${baseUrl}/products`);
+    const products = productsRes.data;
+    setProducts([...products]);
+    console.log(products);
+  };
+
+  const fetchSuppliers = async () => {
+    const suppliersRes = await axios(`${baseUrl}/suppliers`);
+    const suppliers = suppliersRes.data;
+    setSupplier([...suppliers]);
+    console.log(suppliers);
+  };
+
+  const addProduct = (productOption) => {
+    let product = products.find((product) => product.id == productOption.value);
+    let purchaseProduct = {
+      id: productOption.value,
+      name: product.name,
+      quantity: 1,
+      reorderLevel: 5,
+      purchasePrice: product.buyingPrice,
+    };
+    purchaseProduct.totalCost =
+      purchaseProduct.quantity * purchaseProduct.purchasePrice;
+    setPurchaseProducts([...purchaseProducts, purchaseProduct]);
+    setSelectedProduct(null);
+  };
+
+  useEffect(() => {
+    let total = 0;
+    purchaseProducts.forEach((purchaseProduct) => {
+      total += purchaseProduct.totalCost;
+    });
+
+    setGrandTotal(total);
+  }, [purchaseProducts]);
 
   const columns = [
     {
       title: "Product Name",
-      dataIndex: "productName",
+      dataIndex: "name",
       render: (text, record) => (
         <div className="productimgname">
-          <Link className="product-img">
-            <img alt="" src={record.image} />
-          </Link>
-          <Link style={{ fontSize: "15px", marginLeft: "10px" }}>
-            {record.productName}
+          <Link style={{ fontSize: "15px", marginLeft: "10px" }} to="#">
+            {record.name}
           </Link>
         </div>
       ),
     },
     {
-      title: "QTY",
-      dataIndex: "qty",
+      title: "Quantity",
+      dataIndex: "quantity",
     },
     {
-      title: "Purchase Price($)",
+      title: "Unit cost",
       dataIndex: "purchasePrice",
+      render: (text) => `Ksh ${text}`,
     },
     {
-      title: "Discount($)",
-      dataIndex: "discount",
-    },
-    {
-      title: "Tax%",
-      dataIndex: "tax",
-    },
-    {
-      title: "Tax Amount($)",
-      dataIndex: "taxAmount",
-    },
-    {
-      title: "Unit Cost($)",
-      dataIndex: "unitCost",
-    },
-    {
-      title: "Total Cost($)",
+      title: "Total Cost",
       dataIndex: "totalCost",
+      render: (text) => `Ksh ${text}`,
     },
     {
-      render: () => (
+      title: "Action",
+      render: (text, record) => (
         <>
+          <Link className="me-3" to="#">
+            <img src={EditIcon} alt="img" onClick={() => handleEdit(record)} />
+          </Link>
           <Link className="delete-set" to="#" onClick={deleteRow}>
             <img src={DeleteIcon} alt="img" />
           </Link>
@@ -144,37 +206,8 @@ const AddPurchase = () => {
                     <label>Supplier Name</label>
                     <div className="row">
                       <div className="col-lg-10 col-sm-10 col-10">
-                        <Select2
-                          className="select"
-                          data={options}
-                          options={{
-                            placeholder: "Category",
-                          }}
-                        />
+                        <Select options={supplierOptions} />
                       </div>
-                      <div className="col-lg-2 col-sm-2 col-2 ps-0">
-                        <div className="add-icon">
-                          <Link to="#">
-                            <img src={Plus} alt="img" />
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="" class="col-sm-2 form-control-label">
-                      Country
-                    </label>
-                    <div class="col-sm-10">
-                      <select
-                        class="form-control selectpicker"
-                        id="select-country"
-                        data-live-search="true"
-                      >
-                        <option data-tokens="china">Chinaa</option>
-                        <option data-tokens="malayasia">Malayasia</option>
-                        <option data-tokens="singapore">Singapore</option>
-                      </select>
                     </div>
                   </div>
                 </div>
@@ -195,64 +228,93 @@ const AddPurchase = () => {
                 <div className="col-lg-3 col-sm-6 col-12">
                   <div className="form-group">
                     <label>Reference No.</label>
-                    <input type="text" readOnly />
+                    <input type="text" value={reffereceNo} readOnly />
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-6 col-12">
                   <div className="form-group">
-                    <label>Status</label>
-                    <Select2
-                      className="select"
-                      data={options2}
-                      options={{
-                        placeholder: "Category",
-                      }}
-                    />
+                    <label>Payment Status</label>
+                    <Select options={paymentStatus} />
                   </div>
                 </div>
                 <div className="col-lg-12 col-sm-6 col-12">
                   <div className="form-group">
                     <label>Product Name</label>
                     <div className="input-groupicon">
-                      <input
-                        type="text"
-                        placeholder="Scan/Search Product by code and select..."
+                      <Select
+                        options={productOptions}
+                        onChange={(option) => addProduct(option)}
+                        value={selectedProduct}
                       />
-                      <div className="addonset">
-                        <img src={Scanner} alt="img" />
-                      </div>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="row">
                 <div className="table-responsive">
-                  <Table
-                    columns={columns}
-                    dataSource={data}
-                    pagination={false}
-                  />
+                  <Table columns={columns} dataSource={purchaseProducts} />
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>{`Edit ${editingPrduct.name}`}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div className="form-group">
+                        <label>Product Name</label>
+                        <div className="input-groupicon">
+                          <input
+                            type="text"
+                            value={editingPrduct.name}
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <label>Product Quantity</label>
+                        <div className="input-groupicon">
+                          <input
+                            type="number"
+                            value={editingPrduct.quantity}
+                            onChange={(e) => handleDetailsChange(e, "quantity")}
+                            className="form-control"
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <label>Unit Cost</label>
+                        <div className="input-groupicon">
+                          <input
+                            type="number"
+                            value={editingPrduct.purchasePrice}
+                            onChange={(e) => handleDetailsChange(e, "purchasePrice")}
+                            className="form-control"
+                          />
+                        </div>
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <button
+                        className="btn btn-secondary btn-sm"
+                        onClick={handleClose}
+                      >
+                        Close
+                      </button>
+                      <button
+                        className="btn btn-primary btn-sm"
+                        onClick={handSaveEdit}
+                      >
+                        Save Changes
+                      </button>
+                    </Modal.Footer>
+                  </Modal>
                 </div>
               </div>
               <div className="row">
                 <div className="col-lg-12 float-md-right">
                   <div className="total-order">
                     <ul>
-                      <li>
-                        <h4>Order Tax</h4>
-                        <h5>$ 0.00 (0.00%)</h5>
-                      </li>
-                      <li>
-                        <h4>Discount </h4>
-                        <h5>$ 0.00</h5>
-                      </li>
-                      <li>
-                        <h4>Shipping</h4>
-                        <h5>$ 0.00</h5>
-                      </li>
                       <li className="total">
                         <h4>Grand Total</h4>
-                        <h5>$ 0.00</h5>
+                        <h5>ksh. {grandTotal}</h5>
                       </li>
                     </ul>
                   </div>
